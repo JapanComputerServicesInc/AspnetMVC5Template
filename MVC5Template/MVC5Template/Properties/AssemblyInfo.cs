47e66dc5703affc93a049eab05f263b52744818f
takuya3753 <maeda_t@japacom.co.jp>
2016-01-13 11:24:27 +0900

    保存用

--- MVC5Template/MVC5Template/Properties/AssemblyInfo.cs
+++ MVC5Template/MVC5Template/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVC5Sample")]
+[assembly: AssemblyTitle("MVC5Template")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("株式会社ジャパンコンピューターサービス")]
-[assembly: AssemblyProduct("MVC5Sample")]
+[assembly: AssemblyProduct("MVC5Template")]
 [assembly: AssemblyCopyright("Copyright © 株式会社ジャパンコンピューターサービス 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
